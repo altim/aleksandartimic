b1499a5e3ddc575a9baa154ff29cb0106eb8233e
Aleksandar Timic <aleksandar.timic@mvpworkshop.co>
2021-07-20 19:54:50 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 console.log('\x1b[1m', '\n\nAleksandar Timic')
 console.log('\x1b[34m', '\nSenior Frontend Developer @ Celsius Network Europe')
-console.log('email: aleksandartimic@gmail.com')
-console.log('business email: aleksandar.timic@celsius.network')
-console.log('phone: +381603364567\n\n')
+console.log('Email: aleksandartimic@gmail.com')
+console.log('Business email: aleksandar.timic@celsius.network')
+console.log('Phone: +381603364567')
+console.log('Linkedin: https://www.linkedin.com/in/aleksandar-timic-a8a77627/\n\n')
