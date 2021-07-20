a2049d2cba5cdd620325fa0e59bf885b8b4e48d7
Aleksandar Timic <aleksandar.timic@mvpworkshop.co>
2021-07-20 19:08:37 +0200

    Visit card

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# aleksandartimic
+# Aleksandar Timic - visit card
+
+To get my visit card just type
+```
+npx aleksandartimic
+```
+
+
---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('\x1b[1m', '\n\nAleksandar Timic')
+console.log('\x1b[34m', '\nSenior Frontend Developer @ Celsius Network Europe')
+console.log('email: aleksandartimic@gmail.com')
+console.log('business email: aleksandar.timic@celsius.network')
+console.log('phone: +381603364567\n\n')
